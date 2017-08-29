cafe8048648425870c51deada6cb3429e6bd9bbe
Tony Foerster <tony@phdata.io>
2017-08-29 12:21:33 -0500

    Remove commented code

--- test/test_validate.py
+++ test/test_validate.py
@@ -4,8 +4,3 @@ def test_validate(sdc):
     execute_cmd("../sdc-tool pipeline import --dest production --pipelineJson testpipeline.json --pipelineId previewpipe")
     execute_cmd("../sdc-tool pipeline validate --host development --pipelineId previewpipe")
 
-# def test_preview_start(sdc):
-#     execute_cmd("../sdc-tool pipeline preview-start --pipelineId firstpipe")
-#
-# def test_preview_stop(sdc):
-#     execute_cmd("../sdc-tool pipeline preview-stop --pipelineId firstpipe")
