7ac3899a27cb9a5f257f2951b6b28f8d25a26f80
Tony Foerster <tony@phdata.io>
2017-08-29 13:48:48 -0500

    Cleanup copy-paste erro madness

--- src/sdc-tool/sdc-tool.py
+++ src/sdc-tool/sdc-tool.py
@@ -125,12 +125,12 @@ def define_pipeline_args(subparsers):
     validate_parser.set_defaults(func=validate_command)
 
     # pipeline validate arguments
-    import_parser = pipeline_subparsers.add_parser('validate', help='Validate a pipeline and show issues.')
-    import_parser.add_argument('--host', required=True, dest='host_instance', metavar='host_instance',
+    validate_parser = pipeline_subparsers.add_parser('validate', help='Validate a pipeline and show issues.')
+    validate_parser.add_argument('--host', required=True, dest='host_instance', metavar='host_instance',
                                help='The instance name of the target SDC (must match an instance name in sdc-hosts.yml)')
-    import_parser.add_argument('--pipelineId', required=True, dest='pipeline_id',
+    validate_parser.add_argument('--pipelineId', required=True, dest='pipeline_id',
                                metavar='destinationPipelineId', help='The ID of a pipeline in the source SDC')
-    import_parser.set_defaults(func=validate_command)
+    validate_parser.set_defaults(func=validate_command)
 
 
 def define_system_args(subparsers):
