aef3f19262b43ee11819fe76cffeaabbe5f5c3b9
Tony Foerster <tony@phdata.io>
2017-08-30 14:42:04 -0500

    Add test for invalid pipeline

--- test/test_validate.py
+++ test/test_validate.py
@@ -7,3 +7,10 @@ def test_validate(sdc):
     exit_code, out, err = execute_cmd("../sdc-tool pipeline validate --host development --pipelineId previewpipe")
     out_parsed = json.loads(out)
     assert out_parsed['status'] == "VALID"
+
+def test_invalid_pipeline(sdc):
+    exit_code, out, err = execute_cmd("../sdc-tool pipeline import --dest production --pipelineJson testpipeline-invalid.json --pipelineId previewpipe")
+    assert exit_code == 0
+    exit_code, out, err = execute_cmd("../sdc-tool pipeline validate --host development --pipelineId previewpipe")
+    out_parsed = json.loads(out)
+    assert out_parsed['status'] == "VALIDATION_ERROR"
