a86a1cb838d41e6232d58a2e6ecc792d8c48b840
Tony Foerster <tony@phdata.io>
2017-09-06 21:21:50 -0500

    Add metadata and package for pypi

--- setup.py
+++ setup.py
@@ -9,9 +9,11 @@ setup(name='sdctool',
       description='Streamsets DataCollector API utility',
       author='phData inc',
       author_email='brian@phdata.io, tony@phdata.io',
-      url='phdata.io',
+      url='https://github.com/phdata/sdc-api-tool',
       install_requires=['pyyaml', 'requests', 'pytest'],
       packages=['sdctool'],
-      scripts=['sdc-tool']
+      scripts=['sdc-tool'],
+      download_url = 'https://github.com/phdata/sdc-api-tool/tree/feature/install/dist/sdc-tool-0.9.tar.gz',
+      keywords = ['streamsets', 'api', 'pipeline', 'cli']
      )
 
