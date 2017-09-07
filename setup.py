e525b8542a6f7c49791ded56bbe662b2f79517d9
Tony Foerster <tony@phdata.io>
2017-09-07 10:08:58 -0500

    Improve pypi docs

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+import pypandoc
 
-
+long_description = pypandoc.convert('README.md', 'rst')
 
 setup(name='sdctool',
       version='0.9',
@@ -10,10 +11,11 @@ setup(name='sdctool',
       author='phData inc',
       author_email='brian@phdata.io, tony@phdata.io',
       url='https://github.com/phdata/sdc-api-tool',
-      install_requires=['pyyaml', 'requests', 'pytest'],
+      install_requires=['pyyaml', 'requests', 'pytest', 'pypandoc'],
       packages=['sdctool'],
       scripts=['sdc-tool'],
-      download_url = 'https://github.com/phdata/sdc-api-tool/tree/feature/install/dist/sdc-tool-0.9.tar.gz',
-      keywords = ['streamsets', 'api', 'pipeline', 'cli']
+      download_url = 'https://github.com/phdata/sdc-api-tool/tree/master/dist/sdc-tool-0.9.tar.gz',
+      keywords = ['streamsets', 'api', 'pipeline', 'cli'],
+      long_description=long_description
      )
 
